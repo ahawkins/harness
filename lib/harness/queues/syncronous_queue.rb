6a0e3d12d97cf1e0f25addc48d77dd2f79b134a3
twinturbo <me@broadcastingadam.com>
2012-04-10 15:00:07 +0200

    Catch errors in syncronous queue

--- lib/harness/queues/syncronous_queue.rb
+++ lib/harness/queues/syncronous_queue.rb
@@ -1,7 +1,13 @@
 module Harness
   class SyncronousQueue
     def self.push(measurement)
-      Harness::Job.new.log(measurement)
+      begin
+        Harness::Job.new.log(measurement)
+      rescue LoggingError => ex
+        logger.debug "[Harness] Logging measurement failed! Server Said: #{ex}"
+        logger.debug ex.backtrace.join("\n")
+        logger.warn "[Harness] Could not post measurement! Enable debug logging to see full errors"
+      end
     end
   end
 end
