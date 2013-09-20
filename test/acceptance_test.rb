eb59c789208c59f2bd38f349c6af9cd79e0e45ad
ahawkins <me@broadcastingadam.com>
2013-09-21 01:24:41 +0200

    move test case into an acceptance test

---
+++ test/acceptance_test.rb
@@ -0,0 +1,23 @@
+require_relative 'test_helper'
+
+class AcceptanceTest < MiniTest::Unit::TestCase
+  def test_works_with_actual_statsd
+    Harness.config.collector = Statsd.new
+    Harness.config.queue = Harness::SynchronousQueue.new
+
+    Harness.increment 'foo', 0.6
+    Harness.count 'foo', 5, 0.5
+
+    Harness.gauge 'foo', 0.5, 0.1
+
+    Harness.timing 'foo', 0.5, 0.1
+
+    Harness.time 'foo', 1 do
+      true
+    end
+
+    Harness.instrument 'foo', 1 do
+      true
+    end
+  end
+end
