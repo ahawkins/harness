06e01950e78189643333b77fe8297bf1e02bd923
ahawkins <me@broadcastingadam.com>
2013-09-24 00:54:21 +0200

    Use field instead of featch (fix 1.9 compat)

--- lib/harness/gauges/haproxy_gauge.rb
+++ lib/harness/gauges/haproxy_gauge.rb
@@ -19,35 +19,35 @@ module Harness
       response = http.request request
 
       CSV.parse(response.body, headers: true) do |row|
-        pxname = row.fetch 'pxname'
-        server = row.fetch('svname').downcase
+        pxname = row.field 'pxname'
+        server = row.field('svname').downcase
 
-        gauge "haproxy.#{pxname}.queue.current.#{server}", row.fetch('qcur').to_i
-        gauge "haproxy.#{pxname}.queue.max.#{server}", row.fetch('qmax').to_i
+        gauge "haproxy.#{pxname}.queue.current.#{server}", row.field('qcur').to_i
+        gauge "haproxy.#{pxname}.queue.max.#{server}", row.field('qmax').to_i
 
-        gauge "haproxy.#{pxname}.session.current.#{server}", row.fetch('scur').to_i
-        gauge "haproxy.#{pxname}.session.max.#{server}", row.fetch('smax').to_i
-        gauge "haproxy.#{pxname}.session.limit.#{server}", row.fetch('slim').to_i
-        gauge "haproxy.#{pxname}.session.total.#{server}", row.fetch('stot').to_i
+        gauge "haproxy.#{pxname}.session.current.#{server}", row.field('scur').to_i
+        gauge "haproxy.#{pxname}.session.max.#{server}", row.field('smax').to_i
+        gauge "haproxy.#{pxname}.session.limit.#{server}", row.field('slim').to_i
+        gauge "haproxy.#{pxname}.session.total.#{server}", row.field('stot').to_i
 
-        gauge "haproxy.#{pxname}.bytes.in.#{server}", row.fetch('bin').to_i
-        gauge "haproxy.#{pxname}.bytes.out.#{server}", row.fetch('bout').to_i
+        gauge "haproxy.#{pxname}.bytes.in.#{server}", row.field('bin').to_i
+        gauge "haproxy.#{pxname}.bytes.out.#{server}", row.field('bout').to_i
 
-        gauge "haproxy.#{pxname}.request.denied.#{server}", row.fetch('dreq').to_i
-        gauge "haproxy.#{pxname}.request.error.#{server}", row.fetch('ereq').to_i
+        gauge "haproxy.#{pxname}.request.denied.#{server}", row.field('dreq').to_i
+        gauge "haproxy.#{pxname}.request.error.#{server}", row.field('ereq').to_i
 
-        gauge "haproxy.#{pxname}.response.1xx.#{server}", row.fetch('hrsp_1xx').to_i
-        gauge "haproxy.#{pxname}.response.2xx.#{server}", row.fetch('hrsp_2xx').to_i
-        gauge "haproxy.#{pxname}.response.3xx.#{server}", row.fetch('hrsp_3xx').to_i
-        gauge "haproxy.#{pxname}.response.4xx.#{server}", row.fetch('hrsp_4xx').to_i
-        gauge "haproxy.#{pxname}.response.5xx.#{server}", row.fetch('hrsp_5xx').to_i
+        gauge "haproxy.#{pxname}.response.1xx.#{server}", row.field('hrsp_1xx').to_i
+        gauge "haproxy.#{pxname}.response.2xx.#{server}", row.field('hrsp_2xx').to_i
+        gauge "haproxy.#{pxname}.response.3xx.#{server}", row.field('hrsp_3xx').to_i
+        gauge "haproxy.#{pxname}.response.4xx.#{server}", row.field('hrsp_4xx').to_i
+        gauge "haproxy.#{pxname}.response.5xx.#{server}", row.field('hrsp_5xx').to_i
 
-        gauge "haproxy.#{pxname}.request.rate.#{server}", row.fetch('req_rate').to_i
-        gauge "haproxy.#{pxname}.request.max_rate.#{server}", row.fetch('req_rate_max').to_i
+        gauge "haproxy.#{pxname}.request.rate.#{server}", row.field('req_rate').to_i
+        gauge "haproxy.#{pxname}.request.max_rate.#{server}", row.field('req_rate_max').to_i
 
-        gauge "haproxy.#{pxname}.request.total.#{server}", row.fetch('req_tot').to_i
+        gauge "haproxy.#{pxname}.request.total.#{server}", row.field('req_tot').to_i
 
-        gauge "haproxy.#{pxname}.downtime.#{server}", row.fetch('downtime').to_i
+        gauge "haproxy.#{pxname}.downtime.#{server}", row.field('downtime').to_i
       end
     end
   end
